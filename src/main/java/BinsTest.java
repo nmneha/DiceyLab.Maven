496aa917d1b22b756725e82f470cf1d6f712cd64
nusera <nusera.neha@gmail.com>
2022-03-18 22:01:15 -0400

    created bins class and tests

---
+++ src/main/java/BinsTest.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinsTest {
+    Bins results = new Bins(2, 12);
+
+
+    @Test
+    void getBin() {
+        //Given
+        int expected = 0;
+
+        System.out.println(results.getBin(10));
+        int actual =  results.getBin(10);
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    void increment() {
+       // given
+        int expected = 20;
+        System.out.println("Before: " + results.getBin(10));
+
+        results.increment(10);
+        results.increment(10);
+
+        System.out.println("After: " + results.getBin(10));
+        int actual = results.getBin(10);
+
+    }
+}
